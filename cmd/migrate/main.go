0e02beb5c483f5d03964bd704a36493d14ba186a
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-04-10 13:41:29 -0400

    fix migrate env name

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -13,6 +13,7 @@ func main() {
 	chConfig := getClickhouseENV()
 	if err := migrations.RunGoose(ctx, os.Args[1:], chConfig); err != nil {
 		fmt.Printf("failed to run goose: %v\n", err)
+		fmt.Printf("Clickhouse Host %s, Port %s, User %s\n", chConfig.Host, chConfig.Port, chConfig.User)
 		os.Exit(1)
 	}
 }
@@ -22,7 +23,7 @@ func getClickhouseENV() migrations.ClickhouseConfig {
 	if !ok {
 		host = "localhost"
 	}
-	port, ok := os.LookupEnv("CLICKHOUSE_TCP_PORT")
+	port, ok := os.LookupEnv("CLICKHOUSE_PORT")
 	if !ok {
 		port = "9000"
 	}
