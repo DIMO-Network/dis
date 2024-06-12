e86968c4af687636c7ad775fc01cfc854e3c5a43
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-06-12 08:24:07 -0400

    Adds a new fingerprint_indexer processor.
    * The processor is responsible for creating an indexable name for
      fingerprint messages.

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.22.0
 require (
 	github.com/DIMO-Network/devices-api v1.25.16
 	github.com/DIMO-Network/model-garage v0.2.1
+	github.com/DIMO-Network/nameindexer v0.0.0-20240612012111-22cbf394a69f
 	github.com/DIMO-Network/shared v0.10.10
 	github.com/benthosdev/benthos/v4 v4.27.0
-	github.com/ethereum/go-ethereum v1.14.3
+	github.com/ethereum/go-ethereum v1.14.5
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pressly/goose v2.7.0+incompatible
 	github.com/stretchr/testify v1.9.0
@@ -50,7 +51,7 @@ require (
 	github.com/Jeffail/shutdown v1.0.0 // indirect
 	github.com/JohnCGriffin/overflow v0.0.0-20211019200055-46fa312c352c // indirect
 	github.com/Masterminds/squirrel v1.5.4 // indirect
-	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/PaesslerAG/gval v1.2.2 // indirect
 	github.com/PaesslerAG/jsonpath v0.1.1 // indirect
@@ -110,7 +111,7 @@ require (
 	github.com/bwmarrin/discordgo v0.27.1 // indirect
 	github.com/bwmarrin/snowflake v0.3.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/clbanning/mxj/v2 v2.7.0 // indirect
 	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
 	github.com/colinmarc/hdfs v1.1.3 // indirect
@@ -325,17 +326,17 @@ require (
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/oauth2 v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/term v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	golang.org/x/tools v0.20.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/api v0.162.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
---
+++ internal/fingerprintindexer/fingerprint_indexer_test.go
@@ -0,0 +1,109 @@
+package fingerprintindexer_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/DIMO-Network/benthos-plugin/internal/fingerprintindexer"
+	"github.com/DIMO-Network/nameindexer"
+	"github.com/benthosdev/benthos/v4/public/service"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFingerprintIndexerProcessor(t *testing.T) {
+	processor := &fingerprintindexer.Processor{}
+	ctx := context.Background()
+
+	tests := []struct {
+		name         string
+		jsonString   string
+		expectedMeta string
+		expectErr    bool
+	}{
+		{
+			name: "Valid fingerprint message with default fillers",
+			jsonString: `{
+				"time": "2024-06-11T15:30:00Z",
+				"subject": "0xc57d6d57fca59d0517038c968a1b831b071fa679"
+			}`,
+			expectedMeta: mustEncode(&nameindexer.Index{
+				Timestamp: time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				Address:   common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				DataType:  "FP/v0.0.1",
+			}),
+			expectErr: false,
+		},
+		{
+			name:         "Invalid JSON message",
+			jsonString:   `invalid json`,
+			expectedMeta: "",
+			expectErr:    true,
+		},
+		{
+			name: "Invalid subject (address)",
+			jsonString: `{
+				"time": "2024-06-11T15:30:00Z",
+				"subject": "invalid_address"
+			}`,
+			expectedMeta: mustEncode(&nameindexer.Index{
+				Timestamp: time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				Address:   common.HexToAddress(""),
+				DataType:  "FP/v0.0.1",
+			}),
+			expectErr: false,
+		},
+		{
+			name: "Missing subject",
+			jsonString: `{
+				"time": "2024-06-11T15:30:00Z"
+			}`,
+			expectedMeta: mustEncode(&nameindexer.Index{
+				Timestamp: time.Date(2024, 6, 11, 15, 30, 0, 0, time.UTC),
+				Address:   common.HexToAddress(""),
+				DataType:  "FP/v0.0.1",
+			}),
+			expectErr: false,
+		},
+
+		{
+			name: "Missing Time", // this test only works if the test is run in the same second as this declaration
+			jsonString: `{
+				"subject": "0xc57d6d57fca59d0517038c968a1b831b071fa679"
+			}`,
+			expectedMeta: mustEncode(&nameindexer.Index{
+				Timestamp: time.Now(),
+				Address:   common.HexToAddress("0xc57d6d57fca59d0517038c968a1b831b071fa679"),
+				DataType:  "FP/v0.0.1",
+			}),
+			expectErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			msg := service.NewMessage([]byte(tt.jsonString))
+			batch, err := processor.Process(ctx, msg)
+			if tt.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, batch)
+				require.Len(t, batch, 1)
+				encodedIndex, ok := batch[0].MetaGet("index")
+				require.Truef(t, ok, "index meta not found in message: %v", batch[0])
+				require.Equal(t, tt.expectedMeta, encodedIndex)
+			}
+		})
+	}
+}
+
+func mustEncode(index *nameindexer.Index) string {
+	encodedIndex, err := nameindexer.EncodeIndex(index)
+	if err != nil {
+		panic(err)
+	}
+	return encodedIndex
+}
