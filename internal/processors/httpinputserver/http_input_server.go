d0abe493a985d9fc666a4bd9f3392e92f11d1721
Allyson-English <allyson.english@gmail.com>
2025-03-20 11:04:33 -0400

    add attestation server

--- internal/processors/httpinputserver/http_input_server.go
+++ internal/processors/httpinputserver/http_input_server.go
@@ -1,15 +1,26 @@
 package httpinputserver
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/DIMO-Network/dis/internal/processors/httpinputserver/dex"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/redpanda-data/benthos/v4/public/components/io"
+	"google.golang.org/protobuf/proto"
 )
 
-const DIMOConnectionIdKey = "dimo_cloudevent_source"
+// TODO(ae):
+const DIMOCloudEventSource = "cloud_event_source"
+const DIMOConnectionIdKey = "dimo_connection_source"
+const DIMOAttestationIdKey = "dimo_attestation_source"
 
 func init() {
 	io.RegisterCustomHTTPServerInput("dimo_http_server", CertRoutingMiddlewarefunc)
+	io.RegisterCustomHTTPServerInput("dimo_attestation_server", AttestationMiddlewarefunc)
 }
 
 func CertRoutingMiddlewarefunc(r *http.Request) (map[string]any, error) {
@@ -19,5 +30,37 @@ func CertRoutingMiddlewarefunc(r *http.Request) (map[string]any, error) {
 		firstCert := certChain[0]
 		retMeta[DIMOConnectionIdKey] = firstCert.Subject.CommonName
 	}
+	return retMeta, nil
+}
+
+func AttestationMiddlewarefunc(r *http.Request) (map[string]any, error) {
+	retMeta := map[string]any{}
+	userStr := r.Header.Get("user")
+	var token jwt.Token
+	if err := json.Unmarshal([]byte(userStr), &token); err != nil {
+		return retMeta, fmt.Errorf("failed to pull token from request header: %w", err)
+	}
+
+	subj, err := token.Claims.GetSubject()
+	if err != nil {
+		return retMeta, fmt.Errorf("failed to get subject from token claims")
+	}
+
+	decoded, err := base64.RawURLEncoding.DecodeString(subj)
+	if err != nil {
+		return retMeta, fmt.Errorf("failed to decode subject")
+	}
+
+	var user dex.User
+	if err = proto.Unmarshal(decoded, &user); err != nil {
+		return retMeta, fmt.Errorf("failed to parse subject")
+	}
+
+	if !common.IsHexAddress(subj) {
+		return retMeta, fmt.Errorf("subject is not valid hex address")
+	}
+
+	retMeta[DIMOAttestationIdKey] = common.HexToAddress(subj)
+
 	return retMeta, nil
 }
