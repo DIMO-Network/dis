27cebc9b1f10cfc3d20288169ae08ec466b12f3b
Allyson-English <allyson.english@gmail.com>
2025-06-24 14:39:41 -0400

    log

--- internal/processors/cloudeventconvert/attesation_msg.go
+++ internal/processors/cloudeventconvert/attesation_msg.go
@@ -31,7 +31,7 @@ func (c *cloudeventProcessor) processAttestationMsg(ctx context.Context, msg *se
 		return service.MessageBatch{msg}
 	}
 
-	c.logger.Info(fmt.Sprintf("signature valid. source: %s", source))
+	c.logger.Info(fmt.Sprintf("signature valid: %v source: %s", validSignature, source))
 
 	if !validSignature {
 		processors.SetError(msg, processorName, "message signature invalid", nil)
